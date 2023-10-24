aa6a8a18034b14a70031a40cfec7625b0fc8fd26
ivan <ivan.brizo-diaz@outlook.com>
2023-10-24 13:30:56 -0400

    initial commit

---
+++ src/cpsc2150/banking/Mortgage.java
@@ -0,0 +1,9 @@
+package cpsc2150.banking;
+
+import cpsc2150.banking.models.AbsMortgage;
+import cpsc2150.banking.models.IMortgage;
+
+public class Mortgage extends AbsMortgage implements IMortgage {
+
+}
+
